7de439c7b5a5677dd20e34aea3c26d35f40f9ad6
DonutLord2000 <ioripajo2003@gmail.com>
2025-01-10 19:05:18 +0800

    updated news post to be interactive

--- ailumni/resources/views/dashboard.blade.php
+++ ailumni/resources/views/dashboard.blade.php
@@ -206,12 +206,15 @@
             @endif
 
             <!-- NEws post -->
-            <div class="mt-10">
+            <div class="py-12">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                        <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
                 <h2 class="text-2xl font-semibold text-gray-800 mt-4 mb-4 text-center mx-auto">Latest News</h2>
 
                 @foreach ($newsPosts as $post)
                 
-                    <div class="mb-8 relative border-l-4 border-l-gray-200 bg-white shadow-md rounded-lg overflow-hidden mx-auto" style="width: 70rem">
+                    <div class="mb-8 relative border-l-4 border-l-gray-200 bg-white shadow-md rounded-lg overflow-hidden mx-auto">
                         <div class="absolute left-1 top-1 text-black-600">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 " viewBox="0 0 20 20" fill="currentColor">
                                 <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
@@ -241,9 +244,8 @@
                         @endif
                     </div>
                 @endforeach
+                 </div>
             </div>
-            </div>
-        </div>
         </div>
     </div>
 
